055f88d569c18328084b23e0afb16ad6a1217b6d
Thirumalaivasan-Nagarajan <thirumalaivasann.al24@bitsathy.ac.in>
2026-01-21 10:43:12 +0530

    Final todo List web Using CSS Grid

--- javascript/ToDoListweb.js
+++ javascript/ToDoListweb.js
@@ -1,23 +1,28 @@
-const todoList=[];
-function addTask()
-{
-  const inputObject=document.querySelector('.js-input');
-  const dateObject=document.querySelector('.js-date-time');
-  let task=inputObject.value ;
-  let date=dateObject.value ;
-  inputObject.value='';
-  dateObject.value='';
-  todoList.push({task,date});
+const todoList = [];
+function addTask() {
+  const inputObject = document.querySelector('.js-input');
+  const dateObject = document.querySelector('.js-date-time');
+  let task = inputObject.value;
+  let date = dateObject.value;
+  inputObject.value = '';
+  dateObject.value = '';
+  todoList.push({ task, date }); /*Passing object an value into an array using shorthand property*/
 }
-function display()
-{
-  let HTML='';
- for(let i=0;i<todoList.length;i++)
- {
-  let {task,date}=todoList[i];
-  let todo=`<p>${task}    ${date} <button class="delete-button" onclick="todoList.splice(${i},1);display();">Delete</button> </p> `;
-  HTML+=todo;
- }
- console.log(HTML);
- document.querySelector('.container').innerHTML=HTML;
+function display() {
+  let HTML = '';
+  for (let i = 0; i < todoList.length; i++) {
+    let { task, date } = todoList[i];
+    let todo = `
+  <div>
+  ${task}
+  </div>
+  <div>
+   ${date}
+   </div>
+  <button onclick="todoList.splice(${i},1);display();" class="delete-button" >Delete</button>
+  `;
+    HTML += todo;
+  }
+  console.log(HTML);
+  document.querySelector('.container').innerHTML = HTML;
 }
